8f4fea72dfba6ae89a3558cc4a501876a4679886
StevenMcgrew <stevenmcgrew8224@msn.com>
2022-05-11 13:03:43 -0700

    Header and Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer>
+        <a target="_blank" href="https://icons8.com/icon/RNsqmCP8joVQ/calculator">Calculator</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <img src="@/assets/calculator-48x48.png" alt="An image of a calculator">
+    </header>
+</template>
+
+<style scoped>
+
+</style>
