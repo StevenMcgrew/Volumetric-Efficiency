674cf332f4cb3c02a81d2c6f04c7a0510d44c1b3
StevenMcgrew <stevenmcgrew8224@msn.com>
2022-05-14 11:04:20 -0700

    Units Selector Component

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import CalcInput from './CalcInput.vue'
+import UnitsSelector from './UnitsSelector.vue'
 
 </script>
 
@@ -8,25 +9,23 @@ import CalcInput from './CalcInput.vue'
     <div>
         <form id="calcForm">
             <CalcInput :name="'rpm'" :label="'Engine Speed'" :units="'RPM'" :min="0" :max="99999" />
+
             <CalcInput :name="'maf'" :label="'Mass Air Flow'" :min="0" :max="9999" :isLineBreak="false"/>
-            <label for="mafUnits" class="screen-reader-only">Air Flow Units of Measurement</label>
-            <select name="mafUnits" id="mafUnits">
-                <option value="g/s">g/s</option>
-                <option value="kg/s">kg/s</option>
-            </select><br>
+            <UnitsSelector :name="'mafUnits'" :label="'Air Flow Units of Measurement'"
+                :options="[{ value: 'g/s', text: 'g/s' },
+                           { value: 'kg/s', text: 'kg/s' }]" />
+
             <CalcInput :name="'engSize'" :label="'Engine Size'" :units="'L'" :min="0" :max="999" />
+
             <CalcInput :name="'iat'" :label="'Intake Air Temp'" :min="0" :max="200" :isLineBreak="false" />
-            <label for="iatUnits" class="screen-reader-only">Air Temperature Units of Measurement</label>
-            <select name="iatUnits" id="iatUnits">
-                <option value="F">°F</option>
-                <option value="C">°C</option>
-            </select><br>
+            <UnitsSelector :name="'iatUnits'" :label="'Air Temperature Units of Measurement'"
+                :options="[{ value: 'F', text: '°F' },
+                           { value: 'C', text: '°C' }]" />
+
             <CalcInput :name="'elevation'" :label="'Elevation'" :min="-1500" :max="30000" :isLineBreak="false" />
-            <label for="elevationUnits" class="screen-reader-only">Elevation Units of Measurements</label>
-            <select name="elevationUnits" id="elevationUnits">
-                <option value="ft">ft</option>
-                <option value="m">m</option>
-            </select><br>
+            <UnitsSelector :name="'elevationUnits'" :label="'Elevation Units of Measurements'"
+                :options="[{ value: 'ft', text: 'ft' },
+                           { value: 'm', text: 'm' }]" />
         </form>
     </div>
 </template>
@@ -38,38 +37,5 @@ form {
     margin: 0;
     border-radius: 8rem;
     box-shadow: 2rem 2rem 6px 0px grey;
-}
-
-.screen-reader-only {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    margin: -1px;
-    overflow: hidden;
-    clip: rect(0, 0, 0, 0);
-    white-space: nowrap;
-    border: 0;
-}
-
-label {
-    display: inline-block;
-    width: 130rem;
-    padding: 6rem 0rem;
-}
-
-input {
-    width: 75rem;
-    text-align: right;
-    font-size: 14rem;
-}
-
-select {
-    height: 30rem;
-    width: 46rem;
-}
-
-span {
-    padding-left: 4rem;
 }
 </style>
--- src/components/UnitsSelector.vue
+++ src/components/UnitsSelector.vue
@@ -3,17 +3,34 @@
 let props = defineProps({
     name: String,
     label: String,
+    options: Array,
 })
 
 </script>
 
 <template>
-    <label :for="name">{{ label }}</label>
-    <input :id="name" :name="name" type="number" :min="min" :max="max" />
-    <span>{{ units }}</span>
-    <br :class="{ remove: !isLineBreak }">
+    <label :for="name" class="screen-reader-only">{{ label }}</label>
+    <select :name="name" :id="name">
+        <option v-for="option in options" :value="option.value">{{ option.text }}</option>
+    </select>
+    <br>
 </template>
 
 <style scoped>
+.screen-reader-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border: 0;
+}
 
+select {
+    height: 30rem;
+    width: 46rem;
+}
 </style>
